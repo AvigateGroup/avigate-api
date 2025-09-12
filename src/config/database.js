01e12b353f929ee25d7d92d311dfb05f19ae1def
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-09-12 09:25:11 +0100

    remove test account

--- src/config/database.js
+++ src/config/database.js
@@ -26,22 +26,6 @@ module.exports = {
     }
   },
   
-  test: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_TEST_NAME || 'avigate_test',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  },
-  
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
