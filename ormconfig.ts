c8d211a9598491e84536fb98c6a51fd715b68fcf
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-10-05 17:36:31 +0100

    Create TypeORM Config File

---
+++ ormconfig.ts
@@ -0,0 +1,21 @@
+// ormconfig.ts
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config(); // Load .env file
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false,
+});
