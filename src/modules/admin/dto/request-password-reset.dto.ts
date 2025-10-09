67d492393eba7d619fc744f05a33ed9f4a0edff7
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-10-09 09:35:35 +0100

    admin otp and password dto

---
+++ src/modules/admin/dto/request-password-reset.dto.ts
@@ -0,0 +1,11 @@
+// ==================== PASSWORD DTOs ====================
+
+// src/modules/admin/dto/request-password-reset.dto.ts
+import { IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RequestPasswordResetDto {
+  @ApiProperty({ example: 'admin@avigate.co' })
+  @IsEmail()
+  email: string;
+}
