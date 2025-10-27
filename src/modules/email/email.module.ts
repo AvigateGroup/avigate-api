777efa0154724b6e87a6f7ef67b8dc9354ed718b
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-10-27 11:29:58 +0100

    email module that exports the trip email service

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserEmailService } from './user-email.service';
 import { AdminEmailService } from './admin-email.service';
+import { UserTripEmailService } from './user-trip-email.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [UserEmailService, AdminEmailService],
-  exports: [UserEmailService, AdminEmailService],
+  providers: [UserEmailService, AdminEmailService, UserTripEmailService],
+  exports: [UserEmailService, AdminEmailService, UserTripEmailService],
 })
 export class EmailModule {}
