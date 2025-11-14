1fdba7b5cd8df714158546985d68e48941a17163
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-14 11:06:51 +0100

    All services properly exported

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -4,10 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { UserEmailService } from './user-email.service';
 import { AdminEmailService } from './admin-email.service';
 import { UserTripEmailService } from './user-trip-email.service';
+import { CommunityEmailService } from './community-email.service';
+import { LocationShareEmailService } from './location-share-email.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [UserEmailService, AdminEmailService, UserTripEmailService],
-  exports: [UserEmailService, AdminEmailService, UserTripEmailService],
+  providers: [UserEmailService, AdminEmailService, UserTripEmailService, CommunityEmailService,
+    LocationShareEmailService,],
+  exports: [UserEmailService, AdminEmailService, UserTripEmailService, CommunityEmailService,
+    LocationShareEmailService,],
 })
 export class EmailModule {}
