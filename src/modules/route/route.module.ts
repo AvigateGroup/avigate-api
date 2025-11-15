217294ebff63052ffde61b47fed655c2a6602edb
Joel Emmanuel <joel.emmanuel@compasso.com.br>
2025-11-15 07:15:07 +0100

    ADDED: Missing service and entities

--- src/modules/route/route.module.ts
+++ src/modules/route/route.module.ts
@@ -1,4 +1,4 @@
-// src/modules/route/route.module.ts (UPDATED WITH EMAIL SERVICE)
+// src/modules/route/route.module.ts (FIXED)
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -9,9 +9,13 @@ import { TripService } from './services/trip.service';
 import { GoogleMapsService } from './services/google-maps.service';
 import { GeofencingService } from './services/geofencing.service';
 import { RouteMatchingService } from './services/route-matching.service';
+import { IntelligentRouteService } from './services/intelligent-route.service';
+import { IntermediateStopHandlerService } from './services/intermediate-stop-handler.service';
+import { SmartStopMatchingService } from './services/smart-stop-matching.service';
 import { TripGateway } from './gateways/trip.gateway';
 import { Route } from './entities/route.entity';
 import { RouteStep } from './entities/route-step.entity';
+import { RouteSegment } from './entities/route-segment.entity';
 import { ActiveTrip } from './entities/active-trip.entity';
 import { Location } from '../location/entities/location.entity';
 import { User } from '../user/entities/user.entity';
@@ -21,7 +25,14 @@ import { EmailModule } from '../email/email.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Route, RouteStep, ActiveTrip, Location, User]),
+    TypeOrmModule.forFeature([
+      Route,
+      RouteStep,
+      RouteSegment,
+      ActiveTrip,
+      Location,
+      User,
+    ]),
     ConfigModule,
     JwtModule.register({}),
     LocationModule,
@@ -35,8 +46,20 @@ import { EmailModule } from '../email/email.module';
     GoogleMapsService,
     GeofencingService,
     RouteMatchingService,
+    IntelligentRouteService, 
+    IntermediateStopHandlerService, 
+    SmartStopMatchingService, 
     TripGateway,
   ],
-  exports: [RouteService, TripService, GoogleMapsService, GeofencingService, RouteMatchingService],
+  exports: [
+    RouteService,
+    TripService,
+    GoogleMapsService,
+    GeofencingService,
+    RouteMatchingService,
+    IntelligentRouteService,
+    IntermediateStopHandlerService, 
+    SmartStopMatchingService, 
+  ],
 })
 export class RouteModule {}
